f60f1a533021e54db3db7949e9d1c9299b2bd313
sonchaegeon <thscorjs1002@naver.com>
2021-11-07 13:38:29 +0900

    📑 :: project init

---
+++ src/main/java/com/github/dsmcrr/crrserver/CrrApplication.java
@@ -0,0 +1,12 @@
+package com.github.dsmcrr.crrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrrApplication.class, args);
+	}
+}
